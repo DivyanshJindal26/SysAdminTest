621cd6e690f7039d42bf6f21800c48dc272f1f77
DivyanshJindal26 <divyanshjindal95.2012@gmail.com>
2025-01-26 12:24:23 +0530

    hosted tip

---
+++ Prob3/Nutrient-tracker/app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  require 'time'
+  private
+
+  def set_time_zone
+    Time.zone = current_user.time_zone
+  end
+end
